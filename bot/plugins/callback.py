18c11947e7753d873f66066519d1d31d13ca615d
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-05-14 16:08:29 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>😉 you got </i> <code>{leng}</code> <i>Results For Your Query👉 </i> <code>{query}</code>"
+    text=f"😉 you got {(len_results)} Results For Your Query👉 <code>{query}</code>, Requested By <b><code>{update.from_user.first_name}</code></b>, '© Draxmovies'(https://t.me/joinchat/TV_lOjIzLBGmSMGi)"  
              
         
     try:
