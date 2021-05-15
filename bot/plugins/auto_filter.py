7f847b6a5576bec4ffed89711aed2b7334ec5f17
Nmk399303 <78946350+Nmk399303@users.noreply.github.com>
2021-05-15 20:29:49 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -121,7 +121,7 @@ async def auto_filter(bot, update):
                             
             results.append(
                 [
-                    InlineKeyboardButton(📁 file_name, url=file_link)
+                    InlineKeyboardButton(f"📁 file_name" , url=file_link)
                     InlineKeyboardButton(file_size, url=file_link)
                 ]
             )
