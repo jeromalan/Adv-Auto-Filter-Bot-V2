bb00303e852c5e7c0930ee3b192ab801d54db97e
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-05-19 08:41:14 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -42,7 +42,20 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + """</code>\n꧁༺-------------༻꧂
+💡 ɢʀᴏᴜᴘ
+
+@Film_zone_group
+
+  
+
+💡 ɢʀᴏᴜᴘ
+
+@Series_xzone
+
+🎯  ᴄʜᴀɴɴᴇʟs
+
+@Film_zone_channels</b>"""")
         
         if file_type == "document":
         
