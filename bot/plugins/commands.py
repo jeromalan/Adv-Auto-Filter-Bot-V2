f140343294423f15bf13b63c49ea44eb02e70751
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-06-02 14:37:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -12,7 +12,7 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-    update_channel = "https://t.me/joinchat/TV_lOjIzLBGmSMGi"
+    update_channel = "@DraXMovies_draZ"
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
