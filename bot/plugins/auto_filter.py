73aedd19db5e869b8ec1d0c2986fc47749fbe905
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-06-05 20:00:06 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -218,7 +218,7 @@ async def auto_filter(bot, update):
             await bot.send_photo(
                 chat_id = update.chat.id,
                 photo="https://telegra.ph/DraX-Movies-05-14",
-                caption=f"😉 you got {(len_results)} Results For Your Query👉 <code>{query}</code> \n\n<b><a href='https://t.me/joinchat/TV_lOjIzLBGmSMGi'>©DraX Movies</a></b>",         
+                caption=f"😉 you got {(len_results)} Results For Your Query👉 <code>{query}</code> \n\n<b><a href='https://t.me/joinchat/TV_lOjIzLBGmSMGi'>©𝗗𝗿𝗮𝗫 𝗠𝗼𝘃𝗶𝗲𝘀</a></b>",         
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
